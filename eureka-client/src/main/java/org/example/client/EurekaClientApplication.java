11c0f6ad0dfc47f51b93fbe06c629a4ecb02b921
Shannon Pamperl <shanman190@gmail.com>
2016-04-19 06:52:18 -0500

    Initial commit

---
+++ eureka-client/src/main/java/org/example/client/EurekaClientApplication.java
@@ -0,0 +1,11 @@
+package org.example.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
